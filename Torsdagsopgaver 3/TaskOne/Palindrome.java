90d01bbca6951f351f1b604c88ae36071b4bd536
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2022-09-21 19:18:56 +0200

    update

---
+++ Torsdagsopgaver 3/TaskOne/Palindrome.java
@@ -0,0 +1,30 @@
+class Palindrome
+{	
+
+	public static void main(String[] args) 
+	{
+		printlfPalindrome("HalLo");
+	}
+
+	static void printlfPalindrome(String sentence)
+	{
+		
+			System.out.print(sentence);
+	}
+}
+
+/*
+String reverseSentence="";
+
+		int sentenceLength = sentence.length();
+
+		for(int i=(sentenceLength-1); i>=0; --i)
+		{
+			reverseSentence = reverseSentence + sentence.charAt(i);
+		}
+
+		if (sentence.toLowerCase().equals(reverseSentence.toLowerCase())) 
+		{
+			System.out.print(sentence);
+		}
+**/
