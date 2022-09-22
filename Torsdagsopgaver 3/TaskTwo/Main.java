9af571f8924163301f1e2c588889c909b3ffa58d
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2022-09-22 08:30:57 +0200

    update

--- Torsdagsopgaver 3/TaskTwo/Main.java
+++ Torsdagsopgaver 3/TaskTwo/Main.java
@@ -2,13 +2,13 @@ class Main
 {
 	public static void main(String[] args) 
 	{
-		printPartOfWord("Hallo", 1, 4);
+		printPartOfWord("Kobenhavn", 1, 4);
 	}
 
 	static void printPartOfWord(String sentence, int index, int subset)
 	{
 
-		System.out.println(sentence.substring(index, subset));
+		System.out.println(sentence.substring(index, subset+1));
 
 	}
 }
